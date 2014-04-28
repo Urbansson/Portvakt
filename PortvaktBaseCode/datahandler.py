4c655e558813d69af4f7961c123267e37323f3cc
Teddy <Teddy@brandts.se>
2014-04-28 14:48:00 +0200

    Added datahandler

--- PortvaktBaseCode/datahandler.py
+++ PortvaktBaseCode/datahandler.py
@@ -1,26 +1,27 @@
-import threading
 import time
 import socket 
 from uuid import getnode as get_mac
 
-
-class HelloThread (threading.Thread):
+class datahandler ():
     def __init__(self, host, port, updaterate):
-        
         threading.Thread.__init__(self)
         self.host = host
         self.port = port
         self.updaterate = updaterate
         self.go = True
         self.id = get_mac()
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def run(self):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        print "Starting " + self.name
-        while self.go:
-            time.sleep(self.updaterate)
-            self.s.sendto(str(self.id),(self.host, self.port))
+    	while self.go
+    		#gather data 
+    		#Stora data in ram and/or on disk
+    		#Wait x minutes before sending the data over to the server'
+    		#repeat 
+
+#recv data
         self.s.close()
+        return 10;
 
     def stop(self):
         self.go = False
