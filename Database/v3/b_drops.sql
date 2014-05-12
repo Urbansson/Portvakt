6138b988e7f359556443f176c93224a2b692e187
Teddy <Teddy@brandts.se>
2014-05-12 15:57:07 +0200

    Tog bort kopior.

--- Database/v3/b_drops.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF exists T_BASESTATION;
-DROP TABLE IF exists T_BASESTATION_INFO;
-DROP TABLE IF EXISTS T_BASESTATION_INET_ADDR;
-drop function if exists addBasestation;
-DROP PROCEDURE IF EXISTS registerBasestation;
-drop view if exists AccountBasestations;
-DROP PROCEDURE IF EXISTS setInetAddr;
-DROP PROCEDURE IF EXISTS checkTimeout;
